80b5ca20833ce9f86f51377a56d852eb56beedbe
demandsupply <giova.zol92@gmail.com>
2025-12-24 18:45:09 +0100

    Add: added splitFunction() and greenScreenFunction()

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -3,6 +3,7 @@ const canvas = document.querySelector(".photo");
 const ctx = canvas.getContext("2d");
 const strip = document.querySelector(".strip"); // put all the images
 const snap = document.querySelector(".snap");
+const rgbControl = document.querySelectorAll("input");
 
 // video from pc to canvas element (take a snapshot from video every 16ms).
 // Feat:
@@ -33,10 +34,6 @@ function takePhoto() {
   strip.appendChild(downloadPic);
 }
 
-// ctx.globalAlpha = 0.1;
-
-// pixels = greenScreen(pixels);
-
 // NOTE: each ImageData object ha a "data" property which return an
 // array (Uint8ClamperedArray) that represents the pixels data.
 // each pixel = 4 RGBA bytes (r-g-b-alpha); each color component has a value from 0 to 255
@@ -59,16 +56,95 @@ function rgbSplit(pixels) {
   let numBytes = pixels.data.length;
 
   for (let i = 0; i < numBytes; i = i + 4) {
-    pixels[i + 1];
-    pixels[i + 2];
-    return pixels;
+    pixels.data[i - 23] = pixels.data[i + 1];
+    pixels.data[i - 14] = pixels.data[i + 2];
+    pixels.data[i - 5] = pixels.data[i + 3];
   }
+  return pixels;
 }
-
-// 2.3 alhpaEffect filter (ghosting effect)
 
 // 2.4 greenScreen effect (pick a color from the background and pull out every color within that specific color range - usually green)
 // use the param on the canva to select the color to pull out
+
+console.log(rgbControl[2].value);
+
+let defaultRangeValues = {
+  rmin: 0,
+  rmax: 0,
+  gmin: 0,
+  gmax: 0,
+  bmin: 0,
+  bmax: 0,
+};
+
+function updateRangeValue() {
+  console.log("value", this.value);
+  console.log("name", this.name);
+
+  defaultRangeValues[this.name] = Number(this.value);
+
+  console.log(defaultRangeValues);
+}
+
+function checkRange(minRange, maxRange, currentValue) {
+  //   console.log("minRange: ", minRange);
+  //   console.log("maxRange: ", maxRange);
+  //   console.log("currentValue: ", currentValue);
+
+  return currentValue >= Number(minRange) && currentValue <= Number(maxRange);
+
+  //   if (currentValue < minRange) return minRange;
+  //   if (currentValue > maxRange) return maxRange;
+}
+
+checkRange(defaultRangeValues.rmin, defaultRangeValues.rmax, 128);
+
+rgbControl.forEach((range) =>
+  range.addEventListener("change", updateRangeValue)
+);
+
+function greenScreen(pixels) {
+  let numBytes = pixels.data.length;
+
+  for (let i = 0; i < numBytes; i = i + 4) {
+    // pixels.data[i] = checkRange(
+    //   defaultRangeValues.rmin,
+    //   defaultRangeValues.rmax,
+    //   pixels.data[i]
+    // );
+    // pixels.data[i + 1] = checkRange(
+    //   defaultRangeValues.gmin,
+    //   defaultRangeValues.gmax,
+    //   pixels.data[i + 1]
+    // );
+    // pixels.data[i + 2] = checkRange(
+    //   defaultRangeValues.bmin,
+    //   defaultRangeValues.bmax,
+    //   pixels.data[i + 2]
+    // );
+
+    if (
+      checkRange(
+        defaultRangeValues.rmin,
+        defaultRangeValues.rmax,
+        pixels.data[i]
+      ) &&
+      checkRange(
+        defaultRangeValues.gmin,
+        defaultRangeValues.gmax,
+        pixels.data[i + 1]
+      ) &&
+      checkRange(
+        defaultRangeValues.bmin,
+        defaultRangeValues.bmax,
+        pixels.data[i + 2]
+      )
+    ) {
+      pixels.data[i + 3] = 0;
+    }
+  }
+  return pixels;
+}
 
 getVideo();
 
@@ -110,15 +186,15 @@ function paintToCanvas() {
       willReadFrequently: true,
     });
 
-    console.log("pixels", pixels);
+    // console.log("pixels", pixels);
 
-    pixels = redEffect(pixels);
+    // pixels = redEffect(pixels);
 
     // pixels = rgbSplit(pixels);
 
     // ctx.globalAlpha = 0.1;
 
-    // pixels = greenScreen(pixels);
+    pixels = greenScreen(pixels);
 
     ctx.putImageData(pixels, 0, 0);
     // console.log("doing");
